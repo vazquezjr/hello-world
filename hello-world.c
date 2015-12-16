c32c7dbddf201fec633b04a020bc0d95967ee30a
Ruben Vazquez <vazquezjr@users.noreply.github.com>
2015-12-16 16:09:11 -0500

    Readded README.md unchanged and added hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,20 @@
+/**   Ruben Vazquez
+ **   12/16/2015
+ **   hello-world.c
+ **   Version 1
+ **   A program that outputs a sentence to the computer screen
+ **/
+
+// Header files necessary for required functions
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function
+int main() {
+
+    // Prints out the following sentence to the screen
+    printf("Hello, World!\n");
+
+    // End of program
+    return 0;
+}
